7e03d633e55ed139e1e175cd347c15b6c22ed666
Mugni <asepabdulmugni22@gmail.com>
2026-03-05 21:08:07 +0700

    feat: add react quill

---
+++ components/layout/editor.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import { useState } from "react";
+import "react-quill-new/dist/quill.snow.css";
+
+const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
+
+export default function Editor() {
+  const [value, setValue] = useState("");
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, false] }],
+      ["bold", "italic", "underline"],
+      [{ color: [] }, { background: [] }],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["link"],
+    ],
+  };
+
+  return (
+    <div className="rounded-lg border bg-background">
+      <ReactQuill
+        theme="snow"
+        value={value}
+        onChange={setValue}
+        modules={modules}
+      />
+    </div>
+  );
+}
