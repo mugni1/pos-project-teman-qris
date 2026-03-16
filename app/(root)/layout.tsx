e3d9c62311bc36948c5a5924c12e732b7d1f26a5
Mugni <asepabdulmugni22@gmail.com>
2026-03-16 11:44:18 +0700

    refactor: improve style

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { AppSidebar } from "@/components/app-sidebar";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
