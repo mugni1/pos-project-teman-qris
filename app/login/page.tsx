ad2186eda57cd6afc57cf6a361f29ee39b272f9a
Mugni <asepabdulmugni22@gmail.com>
2026-03-17 16:39:18 +0700

    feat: improve login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
 import { LoginForm } from "@/components/view/login.view";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "POS eBelanja - Login",
@@ -7,5 +8,9 @@ export const metadata: Metadata = {
 };
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginForm />
+    </Suspense>
+  );
 }
--- components/view/login.view.tsx
+++ components/view/login.view.tsx
@@ -86,7 +86,7 @@ export function LoginForm() {
       toast.error(message.split("_").join(" "));
       router.replace(`/login`);
     }
-  }, [[message]]);
+  }, [message]);
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
